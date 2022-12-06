32db58f0e95987e0b5b1f9e32600aef26cb8b520
Domenic Di Francesco <domenicdifrancesco@gmail.com>
2022-12-06 10:55:20 +0000

    adding day_6

---
+++ day_6.jl
@@ -0,0 +1,20 @@
+using DelimitedFiles
+
+cd("/Users/ddifrancesco/Github/AoC2022"); code = readdlm("day_6_data.txt")[1]
+
+function find_marker(code::SubString{String}, chunk_length::Int = 4)
+    for l in 1:(length(code) - chunk_length)
+        start = l; stop = l + chunk_length - 1; chunk = code[start:stop]
+        if unique(chunk) |> x -> length(x) == chunk_length
+            return [chunk, stop]; break
+        end
+    end
+end
+
+# Part 1: Where is the first start-of-packet marker?
+
+find_marker(code)
+
+# Part 2: Where is the first start-of-message marker?
+
+find_marker(code, 14)
